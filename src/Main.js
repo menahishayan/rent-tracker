1f2781760b1250387c80f44c525225fe1109ac16
menahishayan <menahi.shayan@gmail.com>
2020-09-06 23:53:24 +0530

    icons FA5, onclick

--- src/Main.js
+++ src/Main.js
@@ -7,18 +7,18 @@ import moment from 'moment';
 var db = new DB()
 
 const CircleCondition = props => (
-	<div className='circle-container' style={props.style}>
+	<div className='circle-container' style={props.style} onClick={props.onClick}>
 		<div className="circle" style={props.condition ? {backgroundColor: props.color[0]} : {backgroundColor: props.color[1]}}>
-			<center>{props.condition ? <i className={`fa fa-${props.icon[0]}`}></i> : <i className={`fa fa-${props.icon[1]}`}></i>}</center>
+			<center><b className="fas">{props.condition ? props.icon[0] : props.icon[1]}</b></center>
 		</div>
 		{props.title ? <p style={{marginTop:8}}>{props.title}</p> : null}
 	</div>
 )
 
 const Circle = props => (
-	<div className='circle-container'>
+	<div className='circle-container' style={props.style} onClick={props.onClick}>
 		<div className="circle" style={{backgroundColor: props.color || 'darkgrey'}}>
-			<center><i className={`fa fa-${props.icon}`}></i></center>
+			<center><b className="fas">{props.icon}</b></center>
 		</div>
 		<br/>
 		{props.title ? <p className='name'>{props.title}</p> : null}
@@ -59,7 +59,7 @@ function Main(){
 				   {
 					   db.profiles(building).map((profile,i) => (
 		                  <Fragment key={b*i} style={{display:'inline'}}>
-						  	<CircleCondition title={db.getNickname(profile)} condition={db.getRent(i,true)} color={['#07ab0a','darkgrey']} icon={['check','times']} />
+						  	<CircleCondition title={db.getNickname(profile)} condition={db.getRent(i,true)} color={['#07ab0a','darkgrey']} icon={['\uf00c','\uf00d']} />
 		                  	<br style={i%3===2?{display:'block'}:{display:'none'}}/>
 		                  </Fragment>
 		               ))
@@ -80,7 +80,7 @@ function Main(){
 				<Fragment key={i}>
 					<div style={{display:'inline-flex',width:'100%', marginLeft:'2%',cursor: 'pointer'}}>
 						<div style={{display:'inline-block',width:'20%', marginRight:'8%'}}>
-							<Circle color="#5e09b8" icon="user" />
+							<Circle color="#5e09b8" icon={"\uf007"} />
 						</div>
 						<div style={{display:'inline-block',width:'80%', marginTop:'1.5%'}}>
 							<b>{person.name}</b><br/>
@@ -110,7 +110,7 @@ function Main(){
 							{
 								db.getDoors(building,floor).map((door,d) =>
 									<div style={{width:`${100/db.getDoors(building,floor).length}%`, backgroundColor:`hsl(${48-(f+d)*2}, ${(((f+d+1)/db.getDoors(building,floor).length)*80)+15}%, ${(((f+d)/db.getDoors(building,floor).length)*13)+74}%)`}} className="door">
-										<center><b className="door-label">{door}</b><p className="door-subtitle">{db.getNickname(db.data[`${building}_${floor}_${door}`].profile)}</p></center>
+										<center><b className="door-label"><b className="fa fa-f52b"></b>{door}</b><p className="door-subtitle">{db.getNickname(db.data[`${building}_${floor}_${door}`].profile)}</p></center>
 									</div>
 								)
 							}
