38aeb04379f1679f235039d752680bf069da2199
ruby <rubyseher2002@gmail.com>
2020-09-07 01:23:40 +0530

    details

--- src/Details.js
+++ src/Details.js
@@ -6,10 +6,10 @@ var db = new DB()
 function Details (props) {
 	return(
 		<div>
-			<div className='circle' style={{backgroundColor:'darkgrey'}}>
-				<center><i className="fa fa-user"></i></center>
-			</div>
-			<h3><b>{props.location.state.profile.name}</b></h3>
+
+		   <div class='circle' style={{background:"#5e09b8"}}><b className="fas">{"\uf007"}</b></div>
+		   <h4 style={{}}>{props.location.state.profile.name}</h4>
+
 			<div className='container'></div>
 		</div>
 	);
