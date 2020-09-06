4a5cdc9adece02fcfd2a7d4cc0e5f7f51a97f1d2
menahishayan <menahi.shayan@gmail.com>
2020-09-07 00:42:42 +0530

    custom components

---
+++ src/Components.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Main.css'
+
+export const CircleCondition = props => (
+	<div className='circle-container' style={props.style} onClick={props.onClick}>
+		<div className="circle" style={props.condition ? {backgroundColor: props.color[0]} : {backgroundColor: props.color[1]}}>
+			<center><b className="fas">{props.condition ? props.icon[0] : props.icon[1]}</b></center>
+		</div>
+		{props.title ? <p style={{marginTop:8}}>{props.title}</p> : null}
+	</div>
+)
+
+export const Circle = props => (
+	<div className='circle-container' style={props.style} onClick={props.onClick}>
+		<div className="circle" style={{backgroundColor: props.color || 'darkgrey'}}>
+			<center><b className="fas">{props.icon}</b></center>
+		</div>
+		<br/>
+		{props.title ? <p className='name'>{props.title}</p> : null}
+	</div>
+)
--- src/Main.js
+++ src/Main.js
@@ -4,27 +4,9 @@ import React, { Fragment,useState, useEffect } from 'react';
 import './Main.css'
 import moment from 'moment';
 import { Redirect } from 'react-router';
+import { Circle, CircleCondition } from './Components'
 
 var db = new DB()
-
-const CircleCondition = props => (
-	<div className='circle-container' style={props.style} onClick={props.onClick}>
-		<div className="circle" style={props.condition ? {backgroundColor: props.color[0]} : {backgroundColor: props.color[1]}}>
-			<center><b className="fas">{props.condition ? props.icon[0] : props.icon[1]}</b></center>
-		</div>
-		{props.title ? <p style={{marginTop:8}}>{props.title}</p> : null}
-	</div>
-)
-
-const Circle = props => (
-	<div className='circle-container' style={props.style} onClick={props.onClick}>
-		<div className="circle" style={{backgroundColor: props.color || 'darkgrey'}}>
-			<center><b className="fas">{props.icon}</b></center>
-		</div>
-		<br/>
-		{props.title ? <p className='name'>{props.title}</p> : null}
-	</div>
-)
 
 function Main(){
 	const [redirect,setRedirect]= useState();
