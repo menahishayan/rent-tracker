e12cbd86cad7c02689f996642942339a212dc83b
menahishayan <menahi.shayan@gmail.com>
2020-09-09 23:19:02 +0530

    fix backets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Main from './Main';
-// import Edit from './Edit';
 import Details from './Details';
 import GenerateInvoice from './GenerateInvoice';
-//import AddUser from './AddUser';
 import addUser from './addUser';
-// import AddPay from './AddPay';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -14,12 +11,7 @@ function App() {
          <div className="App">
             <Route exact path="/" component={Main} />
             <Route path="/details" render={(props) => <Details {...props} />} />
-            {// <Route path="/AddUser" component={AddUser}/>
-               // <Route path="/Edit" render={(props) => <Edit {...props} />} />
-             <Route path="/addUser" component={addUser}/>
-              { // <Route path="/Edit" render={(props) => <Edit {...props} />} />
-               // <Route path="/AddPayment" render={(props) => <AddPay {...props} />} />
-            }
+            <Route path="/addUser" component={addUser}/>
             <Route path="/generate-invoice" render={(props) => <GenerateInvoice {...props} />} />
          </div>
       </Router>
