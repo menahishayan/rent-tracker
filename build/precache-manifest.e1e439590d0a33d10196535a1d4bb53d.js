3a9a17a5e6c27d7d612603ad87c5add9efabe2c6
menahishayan <menahi.shayan@gmail.com>
2020-09-09 20:14:49 +0530

    firebase, vscode & build

---
+++ build/precache-manifest.e1e439590d0a33d10196535a1d4bb53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f11ade5666404de5f3c5f2e8b0e6b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c06a38f94f786944aad",
+    "url": "/static/css/main.779ab500.chunk.css"
+  },
+  {
+    "revision": "3ca6cb5f8442be00f1bf",
+    "url": "/static/js/2.0c2bda1f.chunk.js"
+  },
+  {
+    "revision": "caaf4c57a4f1675ce08e85a28d302c5c",
+    "url": "/static/js/2.0c2bda1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c06a38f94f786944aad",
+    "url": "/static/js/main.a64a925e.chunk.js"
+  },
+  {
+    "revision": "a7d420990f67e0dd7971",
+    "url": "/static/js/runtime-main.fe7d5aa8.js"
+  }
+]);
