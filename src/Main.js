3e1999dc530897bb1e85b2ae234f93a3c1c70a75
menahishayan <menahi.shayan@gmail.com>
2020-09-10 00:32:33 +0530

    new invocie overlay

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import './Main.css'
 import moment from 'moment';
 import { Redirect } from 'react-router';
-import { Circle, CircleCondition, Overlay } from './Components'
+import { Circle, CircleCondition, Overlay, SlidingOverlay } from './Components'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import { useForm } from 'react-hook-form'
 
@@ -17,6 +17,7 @@ function Main() {
 	const [selectedTennant, setSelectedTennant] = useState();
 	const [ebOverlay, setEBOverlay] = useState(false);
 	const { register, getValues } = useForm();
+	const [editOtherAmount, setEditOtherAmount] = useState(0);
 
 	useEffect(() => {
 		db.refreshCache()
@@ -38,34 +39,39 @@ function Main() {
 	}
 
 	const getNextPayment = (person) => {
-		let month = moment().diff(moment(person.startdate).startOf('month'),'months')+1
-		if(person.payment_history!==undefined) {
+		let month = moment().diff(moment(person.startdate).startOf('month'), 'months') + 1
+		if (person.payment_history !== undefined) {
 			return {
-				i: month-1,
-				month: moment().subtract(month - person.payment_history.length-1, "M").startOf('month'),
-				amount: db.getExpectedRent(person,month)
+				i: month - 1,
+				month: moment().subtract(month - person.payment_history.length - 1, "M").startOf('month'),
+				amount: db.getExpectedRent(person, month)
 			}
 		}
 		else
 			return {
 				i: 0,
 				month: moment(person.startdate),
-				amount: db.getExpectedRent(person,1)
+				amount: db.getExpectedRent(person, 1)
 			}
 	}
 
 	const getTotalCollection = () => {
-		var sumRent=0 ,sumExpectedRent=0
+		var sumRent = 0, sumExpectedRent = 0
 		db.persons().forEach((person) => {
-			let month=getNextPayment(person)
-			let paid = db.getPaidRent(person,month.i).housing||0
-			sumRent+=paid
-			sumExpectedRent+= paid===0 ? db.getExpectedRent(person,month.i+1).housing : paid
+			let month = getNextPayment(person)
+			let paid = db.getPaidRent(person, month.i).housing || 0
+			sumRent += paid
+			sumExpectedRent += paid === 0 ? db.getExpectedRent(person, month.i + 1).housing : paid
 		});
-		return {current:sumRent,total:sumExpectedRent,percent:(sumRent/sumExpectedRent)*100}
+		return { current: sumRent, total: sumExpectedRent, percent: (sumRent / sumExpectedRent) * 100 }
 	}
 
 	var totalCollection = getTotalCollection()
+
+	const resetEBOverlay = () => {
+		setEBOverlay(false)
+		setEditOtherAmount(0)
+	}
 
 	if (redirect)
 		return <Redirect push to={{
@@ -106,12 +112,12 @@ function Main() {
 			</center>
 			<div className="container">
 				<center>
-					<br/>
+					<br />
 					<h2><b className="fas" style={{ fontSize: 26 }}>{"\uf156"}</b>&nbsp;&nbsp;{totalCollection.current}</h2>
-					<ProgressBar now={totalCollection.percent} style={{borderRadius:'25px', margin:'5% 0', width:'95%'}}/>
-					<small style={{ display: 'inline-block', width: '40%', color:'darkgrey' }}>out of {totalCollection.total}</small>
+					<ProgressBar now={totalCollection.percent} style={{ borderRadius: '25px', margin: '5% 0', width: '95%' }} />
+					<small style={{ display: 'inline-block', width: '40%', color: 'darkgrey' }}>out of {totalCollection.total}</small>
 				</center>
-			<br/>
+				<br />
 			</div>
 			<br />
 			<center>
@@ -152,7 +158,7 @@ function Main() {
 										<div className="floor">
 											{
 												db.getDoors(building, floor).map((door, d) =>
-													<div key={d} onClick={() => {setSelectedTennant({building,floor,door}); setTennantOverlay(true)}} style={{ width: `${100 / db.getDoors(building, floor).length}%`, backgroundColor: `hsl(${48 - (f + d) * 2}, ${(((f + d + 1) / db.getDoors(building, floor).length) * 80) + 15}%, ${(((f + d) / db.getDoors(building, floor).length) * 13) + 74}%)` }} className="door">
+													<div key={d} onClick={() => { setSelectedTennant({ building, floor, door }); setTennantOverlay(true) }} style={{ width: `${100 / db.getDoors(building, floor).length}%`, backgroundColor: `hsl(${48 - (f + d) * 2}, ${(((f + d + 1) / db.getDoors(building, floor).length) * 80) + 15}%, ${(((f + d) / db.getDoors(building, floor).length) * 13) + 74}%)` }} className="door">
 														<center><b className="door-label"><b className="fas">{"\uf52a"}</b>&nbsp;{door}</b><p className="door-subtitle">{db.getNickname(db.data[`${building}_${floor}_${door}`].profile)}</p></center>
 													</div>
 												)
@@ -168,13 +174,13 @@ function Main() {
 				}
 			</div>
 			<br /><br />
-			{	selectedTennant &&
+			{selectedTennant &&
 				<Overlay visible={tennantOverlay} bgClick={() => setTennantOverlay(!tennantOverlay)} height={25}>
 					<h3><b>{db.data[`${selectedTennant.building}_${selectedTennant.floor}_${selectedTennant.door}`].profile.name}</b></h3>
-						{`${selectedTennant.floor === 0 ? 'G' : selectedTennant.floor}0${selectedTennant.door}, #${selectedTennant.building}`}<br/>
-						{'Since ' + moment(db.data[`${selectedTennant.building}_${selectedTennant.floor}_${selectedTennant.door}`].startdate).format("Do MMM, YYYY") }<br/>
-					<button className="overlay-button-mx" style={{marginTop:'5%',backgroundColor:'#ED0034'}}>Vacate</button>&nbsp;&nbsp;
-					<button className="overlay-button-mx" style={{marginTop:'5%',backgroundColor:'#00A4BC'}} onClick={() => {setRedirect('/add-person'); }}>Replace</button>
+					{`${selectedTennant.floor === 0 ? 'G' : selectedTennant.floor}0${selectedTennant.door}, #${selectedTennant.building}`}<br />
+					{'Since ' + moment(db.data[`${selectedTennant.building}_${selectedTennant.floor}_${selectedTennant.door}`].startdate).format("Do MMM, YYYY")}<br />
+					<button className="overlay-button-mx" style={{ marginTop: '5%', backgroundColor: '#ED0034' }}>Vacate</button>&nbsp;&nbsp;
+					<button className="overlay-button-mx" style={{ marginTop: '5%', backgroundColor: '#00A4BC' }} onClick={() => { setRedirect('/add-person'); }}>Replace</button>
 				</Overlay>
 			}
 
@@ -184,18 +190,52 @@ function Main() {
 			<div className="container">
 				<center>
 					<h3>Generate</h3>
-					<button className="overlay-button-mx" style={{marginTop:'5%',backgroundColor:'#006CFF'}} onClick={() => setEBOverlay(true)}>{moment().format("MMMM")}</button>&nbsp;&nbsp;
-					<button className="overlay-button-mx" style={{marginTop:'5%',backgroundColor:'#00A4BC'}}>Quarterly</button>
-					<br/><br/>
+					<button className="overlay-button-mx" style={{ marginTop: '5%', backgroundColor: '#006CFF' }} onClick={() => setEBOverlay(true)}>{moment().format("MMMM")}</button>&nbsp;&nbsp;
+					<button className="overlay-button-mx" style={{ marginTop: '5%', backgroundColor: '#00A4BC' }}>Quarterly</button>
+					<br /><br />
 				</center>
 			</div>
-			<Overlay visible={ebOverlay} bgClick={() => setEBOverlay(!ebOverlay)} height={38}>
-				<h3><b>Electricity</b></h3>
-				<p style={{ marginTop:'-2%' }}>Stairs & Pump</p>
-				<input name="eb" style={{display:'inline-block', color:'black', backgroundColor:'white'}} type='number' pattern="[0-9]*" defaultValue={850} ref={register} className="editable-label-input"/>
-				<br/><br/>
-				<button className="overlay-button" style={{marginTop:'5%',backgroundColor:'#006CFF', color:'white'}} onClick={() => { setRedirectProps(parseInt(getValues("eb"))); setRedirect('/generate-invoice'); }}>Next</button>
-			</Overlay>
+			<SlidingOverlay visible={ebOverlay} bgClick={() => resetEBOverlay()} height={48}>
+				<b className="fas" style={{ fontSize: 22, float: 'right' }} onClick={() => resetEBOverlay()}>{"\uf00d"}</b>
+				<br />
+				<center>
+					<div style={{ display: 'inline-block', width: '420%', transition: '.2s ease', marginLeft: `${-2 - (editOtherAmount * 104)}%`, position: 'relative', zIndex: 10 }}>
+						<div style={{ display: 'inline-block', width: '20%' }}>
+							<h3><b>Electricity</b></h3>
+							<p style={{ marginTop: '-2%' }}>Stairs & Pump</p>
+							<br />
+							<b className="fas" style={{ fontSize: 30, display: 'inline-block' }}>{"\uf156"}</b>
+							<input name="eb" style={{ display: 'inline-block' }} type='number' pattern="[0-9]*" defaultValue={850} ref={register} className="editable-label-input" />
+						</div>
+						<div style={{ display: 'inline-block', marginLeft: '5%', width: '20%' }}>
+							<h3><b>Other Charges</b></h3>
+							<p style={{ marginTop: '-2%' }}>Stairs & Pump</p>
+							<br />
+							<b className="fas" style={{ fontSize: 30, display: 'inline-block' }}>{"\uf156"}</b>
+							<input name="other1" style={{ display: 'inline-block' }} type='number' pattern="[0-9]*" defaultValue={0} ref={register} className="editable-label-input" />
+						</div>
+						<div style={{ display: 'inline-block', marginLeft: '5%', width: '20%' }}>
+							<h3><b>Other Charges</b></h3>
+							<p style={{ marginTop: '-2%' }}>Stairs & Pump</p>
+							<br />
+							<b className="fas" style={{ fontSize: 30, display: 'inline-block' }}>{"\uf156"}</b>
+							<input name="other2" style={{ display: 'inline-block' }} type='number' pattern="[0-9]*" defaultValue={0} ref={register} className="editable-label-input" />
+						</div>
+						<div style={{ display: 'inline-block', marginLeft: '5%', width: '20%' }}>
+							<h3><b>Other Charges</b></h3>
+							<p style={{ marginTop: '-2%' }}>Stairs & Pump</p>
+							<br />
+							<b className="fas" style={{ fontSize: 30, display: 'inline-block' }}>{"\uf156"}</b>
+							<input name="other3" style={{ display: 'inline-block' }} type='number' pattern="[0-9]*" defaultValue={0} ref={register} className="editable-label-input" />
+						</div>
+					</div>
+				</center>
+				<b className="fas" onClick={() => setEditOtherAmount(editOtherAmount === 3 ? 0 : editOtherAmount + 1)} style={{ fontSize: 22, float: 'right', marginRight: '5%', marginTop: '-15%', position: 'relative', zIndex: 15 }}>{"\uf1b2"}</b>
+				<br />
+				<center>
+					<button className="overlay-button" style={{ marginTop: '5%', color: '#006CFF' }} onClick={() => { setRedirectProps(parseInt(getValues("eb"))); setRedirect('/generate-invoice'); }}>Next</button>
+				</center>
+			</SlidingOverlay>
 			<br /><br />
 		</div>
 	)
