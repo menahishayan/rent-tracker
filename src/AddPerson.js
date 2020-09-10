db24df920154e9fb343789d533126040170b56d7
ruby <rubyseher2002@gmail.com>
2020-09-10 11:22:37 +0530

    add user

--- src/AddPerson.js
+++ src/AddPerson.js
@@ -1,4 +1,5 @@
 import React ,{ Fragment, useEffect }from 'react';
+import Button from 'react-bootstrap/Button'
 import { useForm } from "react-hook-form";
 import Form from 'react-bootstrap/Form';
 import DB from './DB';
@@ -14,15 +15,16 @@ function AddPerson(){
 	// const [rent, setrent] = useForm();
 	// const [headCount, setheadCount] = useForm();
 
-	const { register } = useForm({
-   		defaultValues: {
-			Name: "bill",
-   		}
- 	});
+   const { register, handleSubmit } = useForm()
+
+   const testSubmitHandler = (d) => {
+  	console.log(d);
+	}
 
 	return(
 		<Fragment>
-		<br />
+		<Form onSubmit={handleSubmit (d => testSubmitHandler(d))}>
+		<br/>
 		<center>
 			<h3><b className="fas">{"\uf234"}</b><b>&nbsp;&nbsp;Add User</b></h3>
 		</center>
@@ -35,11 +37,13 @@ function AddPerson(){
 				<Form.Control type="text" placeholder="ID" />
 			</Form.Group><br/>
 			<Form.Group ref={register} style={{borderBottom: "2px solid #1055e0"}}>
-				<Form.Control type="text" placeholder="StartDate" />
+				<Form.Control type="date" placeholder="StartDate" />
 			</Form.Group><br/>
 			<Form.Group ref={register} style={{borderBottom: "2px solid #1055e0"}}>
-				<Form.Control type="text" placeholder="Advance" />
+				<Form.Control type="number" placeholder="Advance" />
 			</Form.Group><br/>
+			  <center><Button variant="primary" type="submit">submit</Button></center>
+			</Form>
 		</Form>
 		</Fragment>
 	)
